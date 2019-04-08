74d86ae1dfb2926f43ed0c269f672bd2aa033305
chandler <597649635@qq.com>
2019-04-08 11:25:45 +0800

    减小页面大小

---
+++ resume.js
@@ -0,0 +1,6 @@
+require('./src/js/jqExt'); //扩展的jq函数
+require('./src/less/reset.less'); // 重置默认样式
+require('./src/less/ani.less'); // 动画类
+require('./src/less/public.less'); //公共类
+require('./src/less/swiper.less'); //第所有屏样式
+require('./src/js/main');
--- webpack.config.js
+++ webpack.config.js
@@ -6,38 +6,38 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // 打包前清除原文件夹
 const cleanWebpackPlugin = require('clean-webpack-plugin');
+// 模式
+const isDev = process.env.mode === 'development';
 // css 单独打包
 const extractTextWebpackPlugin = require('extract-text-webpack-plugin');
-module.exports = {
+
+const lessExtract = new extractTextWebpackPlugin({
+    filename: 'css/[name]-[hash:8].css'
+});
+const config = {
     entry: {
-        main: './src/js/index.js',
+        main: './resume.js',
     },
     output: {
-        path: path.resolve(__dirname, 'cdj'),
-        filename: 'js/[name].js',
+        path: path.join(__dirname, 'production'),
+        filename: 'js/[name].js'
     },
     module: {
         rules: [
             {
-                test: /\.less$/,
-                exclude: path.resolve(__dirname, 'node_modules'),
-                use: extractTextWebpackPlugin.extract({
+                test: /\.less$/i,
+                use: lessExtract.extract({
                     fallback: 'style-loader',
                     use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                minimize: true //css压缩
-                            }
-                        },
-                        'postcss-loader',
-                        'less-loader'
-                    ],
+                        { loader: 'css-loader' },
+                        { loader: 'postcss-loader' },
+                        { loader: 'less-loader' }
+                    ]
                 })
             },
             {
                 test: /\.js$/,
-                exclude: path.resolve(__dirname, 'node_modules'),
+                exclude: path.join(__dirname, 'node_modules'),
                 use: 'babel-loader'
             },
             {
@@ -68,43 +68,46 @@ module.exports = {
             },
             {
                 test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-withimg-loader',
-                    }
-                ]
+                use: [{
+                    loader: 'html-loader'
+                }]
             }
         ]
     },
     plugins: [
-        // 每次打包都会清除上一次打包的源文件
-        // new cleanWebpackPlugin(['cdj']),
+        // 将css单独打包 用extractTextWebpackPlugin标签引入
+        lessExtract,
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'index.html',
             inject: 'body',
+            favicon: './src/img/logo.ico',
+            hash: true,
             minify: {
                 removeComments: true,
                 collapseWhitespace: true
             }
-        }),
-        // 代码压缩
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            },
-            comments: false
-        }),
-        // 将css单独打包 用extractTextWebpackPlugin标签引入
-        new extractTextWebpackPlugin({
-            filename: getPath => {
-                return getPath('css/[name].css').replace('css/js', 'css');
-            },
-            allChunks: true
         })
-    ],
-    // webpack 服务器
-    devServer: {
-        port: 8888,
-    }
+    ]
 }
+
+// 如果当前模式为开发模式
+if (isDev) {
+    config.mode = 'development';
+    config.devServer = {
+        port: 8888,
+        host: '0.0.0.0',
+        overlay: true,
+        compress: true,
+        hot: true
+    };
+    config.plugins.push(
+        // 热模块替换功能，可以实现局部刷新，节省等待时间
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    );
+} else {
+    config.mode = 'production';
+    config.plugins.push(new cleanWebpackPlugin(['resume']));
+}
+module.exports = config
